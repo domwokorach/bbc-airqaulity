37398cfeed6d113895b32e50b33a5fe013b49b4e
Dominic Wokorach-O <{dominic.olanya}@mail.com>
2022-07-22 19:34:46 +0100

    Inital commit

---
+++ build/precache-manifest.f9e3dc866f2d1b4367b333c209798d6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2367c22cedac70d2bc",
+    "url": "/bbc-air-quality/static/css/main.1848bed2.chunk.css"
+  },
+  {
+    "revision": "7c2367c22cedac70d2bc",
+    "url": "/bbc-air-quality/static/js/main.3cf99bfa.chunk.js"
+  },
+  {
+    "revision": "feea7ec0eaeb75f981d9",
+    "url": "/bbc-air-quality/static/js/runtime~main.a93dc337.js"
+  },
+  {
+    "revision": "6012287d9590efaa2f71",
+    "url": "/bbc-air-quality/static/css/2.756edec6.chunk.css"
+  },
+  {
+    "revision": "6012287d9590efaa2f71",
+    "url": "/bbc-air-quality/static/js/2.6d2c0c50.chunk.js"
+  },
+  {
+    "revision": "7953e96c5ac0eadcfa6fdfc76c3b2aa7",
+    "url": "/bbc-air-quality/static/media/51-100cig.7953e96c.png"
+  },
+  {
+    "revision": "73c6c2d084d963fd3e9ce268286fb686",
+    "url": "/bbc-air-quality/static/media/101-150cig.73c6c2d0.png"
+  },
+  {
+    "revision": "cf06a9707aea8ead8238a043b193cb32",
+    "url": "/bbc-air-quality/index.html"
+  }
+];
